943f14a1a6cd2468bd25b5b889d9ed945b713738
Mennatullah Shehata <68879340+Menna-Shehata@users.noreply.github.com>
2022-11-04 18:57:49 +0000

    get differential studies only

--- interactive_jupyter_notebook/common_queries.py
+++ interactive_jupyter_notebook/common_queries.py
@@ -17,6 +17,7 @@ SELECT (count(*) as ?Count)
 query_study_list = """
     PREFIX bk: <http://knetminer.org/data/rdf/terms/biokno/>
     PREFIX bka: <http://knetminer.org/data/rdf/terms/biokno/attributes/>
+    PREFIX bkr: <http://knetminer.org/data/rdf/resources/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX dc: <http://purl.org/dc/elements/1.1/>
@@ -38,6 +39,7 @@ query_study_list = """
             agri:evidence ?study.
 
         ?study
+            schema:additionalProperty bkr:gxa_analysis_type_differential;
             dc:title ?studyTitle;
             schema:identifier ?studyAcc.
     } """
