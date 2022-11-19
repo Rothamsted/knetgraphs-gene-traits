f066a44672b5574e927db408b11b5495201add7d
Mennatullah Shehata <68879340+Menna-Shehata@users.noreply.github.com>
2022-11-19 21:46:27 +0000

    Update read_cypher_queries.py

--- interactive_jupyter_notebook/read_cypher_queries.py
+++ interactive_jupyter_notebook/read_cypher_queries.py
@@ -29,41 +29,50 @@ def get_cypher_queries(input_file, ontology):
 
 ### Example:
 
-input_file = "./cypher_queries.txt"
-ontology = "BioProc"
-get_cypher_queries(input_file, ontology)
+# input_file = "cypher_queries.txt"
+# ontology = "BioProc"
+# get_cypher_queries(input_file, ontology)
 
 
-qu_string = """
-MATCH path = (gene_1:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
+qu_string1 = """MATCH path = (gene_1:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [part_of_1_23_d:part_of] -> (coExpCluster_23:CoExpCluster) - [rel:enriched_for] -> (bioProc_4:BioProc) WHERE toFloat(rel.p_adjust)<1E-10 AND gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [de_9_22_d:differentially_expressed] -> (dGES_22:DGES) - [enriched_for_23_4_d:enriched_for] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [rel_1_9:homoeolog|regulates|genetic|physical] - (gene_9:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [enc_1_10_d:enc] -> (protein_10:Protein) - [rel_10_10:h_s_s|ortho|xref*0..1] - (protein_10b:Protein) - [participates_in_10_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [enc_1_10_d:enc] -> (protein_10:Protein) - [rel_10_10:h_s_s|ortho|xref*0..1] - (protein_10b:Protein) - [has_domain_10_11_d:has_domain] -> (protDomain_11:ProtDomain) - [participates_in_11_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
 MATCH path = (gene_1:Gene) - [enc_1_10_d:enc] -> (protein_10:Protein) - [rel_10_10:ortho] - (protein_10b:Protein) <- [enc_10_9_d:enc] - (gene_9:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
-MATCH path = (gene_1:Gene) - [enc_1_10_d:enc] -> (protein_10:Protein) - [rel_10_10:ortho] - (protein_10b:Protein) <- [enc_10_9_d:enc] - (gene_9:Gene) - [rel_9_9_2:genetic|physical] - (gene_9b:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path
-"""
+MATCH path = (gene_1:Gene) - [enc_1_10_d:enc] -> (protein_10:Protein) - [rel_10_10:ortho] - (protein_10b:Protein) <- [enc_10_9_d:enc] - (gene_9:Gene) - [rel_9_9_2:genetic|physical] - (gene_9b:Gene) - [participates_in_9_4_d:participates_in] -> (bioProc_4:BioProc) WHERE gene_1.iri IN $startGeneIris RETURN path"""
 
 rep = ["WHERE gene_1.iri ", "WHERE toFloat(rel.p_adjust)<1E-10 AND gene_1.iri ", "IN $startGeneIris ",
 "gene_1", "gene_9b", "gene_9", "bioProc_4", "coExpCluster_23", "protDomain_11", "de_9_22_d", "dGES_22",
 "enriched_for_23_4_d", "has_domain_10_11_d", "part_of_1_23_d",
 "enc_1_10_d", "enc_10_9_d", "participates_in_9_4_d", "participates_in_11_4_d", "participates_in_10_4_d",
-"rel_1_9", "rel_9_9_2", "rel_10_10", "rel", "protein_10b", "protein_10"]
+"rel_1_9", "rel_9_9_2", "rel_10_10", "rel", "protein_10b", "protein_10", " RETURN path"]
 
 
-for w in rep:
-    qu_string = qu_string.replace(w, "")
-print(qu_string)
+
+# for w in rep:
+#     qu_string = qu_string1.replace(w, "")
+# print(qu_string)
 
 
-"""
-MATCH path = (:Gene) - [:participates_in] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:part_of] -> (:CoExpCluster) - [:enriched_for] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:differentially_expressed] -> (:DGES) - [:enriched_for] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:homoeolog|regulates|genetic|physical] - (:Gene) - [:participates_in] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:enc] -> (:Protein) - [:h_s_s|ortho|xref*0..1] - (:Protein) - [:participates_in] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:enc] -> (:Protein) - [:h_s_s|ortho|xref*0..1] - (:Protein) - [:has_domain] -> (:ProtDomain) - [:participates_in] -> (:BioProc) RETURN path  
-MATCH path = (:Gene) - [:enc] -> (:Protein) - [:ortho] - (:Protein) <- [:enc] - (:Gene) - [:participates_in] -> (:BioProc) RETURN path
-MATCH path = (:Gene) - [:enc] -> (:Protein) - [:ortho] - (:Protein) <- [:enc] - (:Gene) - [:genetic|physical] - (:Gene) - [:participates_in] -> (:BioProc) RETURN path
-"""
+qu_string = """MATCH path = (:Gene) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:part_of] -> (:CoExpCluster) - [:enriched_for] -> (:BioProc)
+MATCH path = (:Gene) - [:differentially_expressed] -> (:DGES) - [:enriched_for] -> (:BioProc)
+MATCH path = (:Gene) - [:homoeolog] - (:Gene) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:regulates|genetic|physical] - (:Gene) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:enc] -> (:Protein) - [:h_s_s|ortho|xref*0..1] - (:Protein) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:enc] -> (:Protein) - [:h_s_s|ortho|xref*0..1] - (:Protein) - [:has_domain] -> (:ProtDomain) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:enc] -> (:Protein) - [:ortho] - (:Protein) <- [:enc] - (:Gene) - [:participates_in] -> (:BioProc)
+MATCH path = (:Gene) - [:enc] -> (:Protein) - [:ortho] - (:Protein) <- [:enc] - (:Gene) - [:genetic|physical] - (:Gene) - [:participates_in] -> (:BioProc)"""
+
+
+### create a cypher query to get each pathway counts
+qu_string = qu_string.replace("path = (:Gene)", "path = (m:Gene)")
+queries_list = qu_string.split("\n")
+
+print("\nCopy the string:\n")
+for q in queries_list:
+    q = "UNION ALL\n" + q + "\nWHERE m.TAXID = $neodash_gene_taxid" + "\nWITH count(path) AS count" + f"\nRETURN '{q.replace('MATCH path = (m', '(')}' AS pathway, count\n"
+    #q = "UNION ALL\n" + q + "\nWHERE m.TAXID = '4565'" + "\nWITH count(path) AS count" + f"\nRETURN '{q.replace('MATCH path = (m', '(')}' AS pathway, count\n"
+    print(q)
